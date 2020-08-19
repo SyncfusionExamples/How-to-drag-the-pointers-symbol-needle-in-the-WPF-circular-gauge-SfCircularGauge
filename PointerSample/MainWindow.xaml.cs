cebabe8dc0649a28e8b254aae51b1b64ef3b5c5d
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-19 13:46:00 +0530

    Sample included

---
+++ PointerSample/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.UI.Xaml.Gauges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointerSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CircularPointer_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            range.EndValue = e.Value;
+        }
+    }
+}
